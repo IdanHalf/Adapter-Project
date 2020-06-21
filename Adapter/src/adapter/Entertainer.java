be13be382a7d43500d5ba67c132e960fd1beaa56
איתי פסקוביץ < @DESKTOP-8N9Q0R8>
2020-06-21 10:36:18 +0300

    Entertainer class

---
+++ Adapter/src/adapter/Entertainer.java
@@ -0,0 +1,43 @@
+package adapter;
+
+public class Entertainer {
+	private String name;
+	private int age;
+	private char rating;
+	public Entertainer(String name,int age,char rating){
+		setName(name);
+		setAge(age);
+		setRating(rating);
+		
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		if(name.length()>1)
+			for(int i=0;i<name.length();i++)
+			{
+				if(!(name.toLowerCase().charAt(i)>'a')&&(name.toLowerCase().charAt(i)>'z'))
+					name="entertainer";
+			}
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		if(age>17&&age<66)
+			this.age=age;
+		else
+			age=25;
+	}
+	public char getRating() {
+		return rating;
+	}
+	public void setRating(char rating) {
+		if(rating>='A'&& rating<='F')
+			this.rating=rating;
+		else
+			rating='D';
+	}
+	
+}
