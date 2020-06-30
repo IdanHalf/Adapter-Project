726e81b5dbed9a8e3aea114acec857e591f34b24
itaypf <67192735+itaypf@users.noreply.github.com>
2020-06-30 19:01:26 +0300

    just making sure its updated

--- AllPlayerAdapter.java
+++ AllPlayerAdapter.java
@@ -2,11 +2,11 @@ package adapter;
 
 public interface AllPlayerAdapter {
 	public String getName();
-	public  String getAge();
+	public String getAge();
 	public String getHeight();
 	public String getRating();
 	public String getDonation();
 	public String getSpecial1();
 	public String getSpecial2();
 	public void print();	
-}
+}	
